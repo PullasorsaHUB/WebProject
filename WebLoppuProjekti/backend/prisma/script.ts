e0b7fb32a83eac012f8d1a41aeeea29ad671c1e1
SkapaJrr <atisk001@edu.xamk.fi>
2025-12-11 20:03:37 +0200

    Lisätty "User", sekä "Favourites" taulut, ja luotu seedi käyttäjä.

--- WebLoppuProjekti/backend/prisma/script.ts
+++ WebLoppuProjekti/backend/prisma/script.ts
@@ -1,7 +1,12 @@
 import {prisma} from './prisma'
 
 async function main() {
-  // Create a new user with a post
+
+  //Remove existing data
+  await prisma.recipe.deleteMany({})
+  await prisma.user.deleteMany({})
+
+  // Create a new recipe
   const recipe = await prisma.recipe.create({
     data: {
         title: 'Spaghetti Bolognese',
@@ -12,18 +17,37 @@ async function main() {
     })
   console.log('Created recipe:', recipe)
 
-  // Fetch all users with their posts
-  const allUsers = await prisma.recipe.findMany({
+  // Create test user
+
+    const user = await prisma.user.create({
+        data: {
+            userName: 'Test User',
+            email: 'test@test.com',
+            passwordHash:'password123'
+        },
+    })
+    console.log('Created user:', user)
+
+  // Fetch all recipes
+  const allRecipes = await prisma.recipe.findMany({
   })
-  console.log('All recipes:', JSON.stringify(allUsers, null, 2))
+  console.log('All recipes:', JSON.stringify(allRecipes, null, 2))
+
+  // Fetch all users
+  const allUsers = await prisma.user.findMany({
+  })
+  console.log('All users:', JSON.stringify(allUsers, null, 2))
+
 }
 
 main()
   .then(async () => {
     await prisma.$disconnect()
+    console.log('Seeding finished.')
   })
   .catch(async (e) => {
     console.error(e)
     await prisma.$disconnect()
+    console.log('Seeding failed.')
     process.exit(1)
   })
